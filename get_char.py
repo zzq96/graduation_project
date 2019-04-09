58505853cd09f61b63060aa6fd8a0156022d5e28
zzq96 <1402806685@qq.com>
2019-04-09 11:11:22 +0800

    Initial commit

---
+++ get_char.py
@@ -0,0 +1,13 @@
+#%%
+import cv2
+import numpy as np
+# %%
+import numpy as np
+
+print(np.array([1, 2]))
+# %%
+data = cv2.imread(r"D:\code\java\py_java\image\1.jpg")
+cv2.imshow("test", data)
+cv2.waitKey()
+cv2.destroyAllWindows()
+# %%
